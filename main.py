dda71771e30f7c265df6870d34017d009d3bbb33
include07 <j.elfirqi@gmail.com>
2023-11-14 22:09:26 +0100

    below bare-minimum working product

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pyautogui as pg #importing the PyAutoGUI package
 import time #time is part of the default Python installation
+pg.FAILSAFE = True
 
-dino_position = pg.locateOnScreen('dino.png') #for this to work, change the dino.png file with your own screenshot
-
-print(dino_position)
+time.sleep(5)
+pg.press('space')
+while True:
+  t = []
+  ss = pg.screenshot(region=(700,750,200,70))
+  for i in range(200):
+    for j in range(70):
+      t.append(ss.getpixel((i,j))==(172,172,172))
+  if any(t):
+    pg.press('space')
+    time.sleep(0.25)
+    pg.keyDown('down')
+    pg.keyUp('down') 
